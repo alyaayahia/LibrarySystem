7c6540cf7518f6ebdab4184f11327b4342e14aae
alyaayahia <ayahia998@gmail.com>
2026-06-02 20:44:44 +0300

    feat(infrastructure): add library db context

---
+++ src/LibrarySystem.Infrastructure/Persistence/LibraryDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Infrastructure.Persistence;
+
+public class LibraryDbContext : DbContext
+{
+    public LibraryDbContext(
+        DbContextOptions<LibraryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+
+    public DbSet<Member> Members { get; set; }
+
+    public DbSet<Borrowing> Borrowings { get; set; }
+}
