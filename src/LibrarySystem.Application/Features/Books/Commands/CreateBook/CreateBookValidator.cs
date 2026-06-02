ec5661d614178d5fd532d39d7b05e4eba41ef79e
alyaayahia <ayahia998@gmail.com>
2026-06-02 20:16:24 +0300

    feat(application): add create book validator

---
+++ src/LibrarySystem.Application/Features/Books/Commands/CreateBook/CreateBookValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentValidation;
+
+namespace LibrarySystem.Application.Features.Books.Commands.CreateBook;
+
+public class CreateBookValidator
+    : AbstractValidator<CreateBookCommand>
+{
+    public CreateBookValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .MaximumLength(200);
+
+        RuleFor(x => x.Author)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.ISBN)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.Genre)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0);
+
+        RuleFor(x => x.PublishedYear)
+            .GreaterThan(1900);
+    }
+}
