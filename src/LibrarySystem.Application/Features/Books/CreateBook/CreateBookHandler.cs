760c5249f6be4adf9c2ee5459a356b9f22665d8b
alyaayahia <ayahia998@gmail.com>
2026-06-02 20:09:52 +0300

    feat(application): add create book handler

---
+++ src/LibrarySystem.Application/Features/Books/CreateBook/CreateBookHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem.Application.Interfaces;
+using LibrarySystem.Domain.Entities;
+using MediatR;
+
+namespace LibrarySystem.Application.Features.Books.Commands.CreateBook;
+
+public class CreateBookHandler
+    : IRequestHandler<CreateBookCommand, Guid>
+{
+    private readonly IBookRepository _bookRepository;
+
+    public CreateBookHandler(
+        IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public async Task<Guid> Handle(
+        CreateBookCommand request,
+        CancellationToken cancellationToken)
+    {
+        var book = new Book(
+            request.Title,
+            request.Author,
+            request.ISBN,
+            request.Genre,
+            request.Price,
+            request.PublishedYear);
+
+        await _bookRepository.AddAsync(book);
+
+        return book.Id;
+    }
+}
