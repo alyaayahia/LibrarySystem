dff61d7af1eb7ddc0fee14b8c567350f8eae3483
alyaayahia <ayahia998@gmail.com>
2026-06-02 17:52:16 +0300

    feat(application): add generic result pattern

---
+++ src/LibrarySystem.Application/Common/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Application.Common;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public T? Value { get; }
+
+    public string? Error { get; }
+
+    private Result(
+        bool isSuccess,
+        T? value,
+        string? error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value)
+    {
+        return new Result<T>(
+            true,
+            value,
+            null);
+    }
+
+    public static Result<T> Failure(string error)
+    {
+        return new Result<T>(
+            false,
+            default,
+            error);
+    }
+}
