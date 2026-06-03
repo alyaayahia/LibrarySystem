5aa538b7273499ab281d6bd8c201b23beebf2c09
alyaayahia <ayahia998@gmail.com>
2026-06-03 15:03:35 +0300

    feat(infrastructure): add member entity configuration

---
+++ src/LibrarySystem.Infrastructure/Persistence/Configurations/BookConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace LibrarySystem.Infrastructure.Persistence.Configurations;
+
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.ToTable("Books");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Author)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.ISBN)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.Genre)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Price)
+            .HasColumnType("decimal(18,2)");
+    }
+}
---
+++ src/LibrarySystem.Infrastructure/Persistence/Configurations/MemberConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibrarySystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibrarySystem.Infrastructure.Persistence.Configurations;
+
+public class MemberConfiguration : IEntityTypeConfiguration<Member>
+{
+    public void Configure(EntityTypeBuilder<Member> builder)
+    {
+        builder.ToTable("Members");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Phone)
+            .HasMaxLength(20);
+
+        builder.HasIndex(x => x.Email)
+            .IsUnique();
+    }
+}
