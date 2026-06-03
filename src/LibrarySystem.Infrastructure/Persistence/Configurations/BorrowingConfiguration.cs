3c7d4ddd4ba93772481f91e9bd00ec2421403886
alyaayahia <ayahia998@gmail.com>
2026-06-03 15:07:07 +0300

    feat(infrastructure): add borrowing entity configuration

---
+++ src/LibrarySystem.Infrastructure/Persistence/Configurations/BorrowingConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibrarySystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibrarySystem.Infrastructure.Persistence.Configurations;
+
+public class BorrowingConfiguration
+    : IEntityTypeConfiguration<Borrowing>
+{
+    public void Configure(
+        EntityTypeBuilder<Borrowing> builder)
+    {
+        builder.ToTable("Borrowings");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.LateFee)
+            .HasColumnType("decimal(18,2)");
+
+        builder.HasOne(x => x.Book)
+            .WithMany()
+            .HasForeignKey(x => x.BookId);
+
+        builder.HasOne(x => x.Member)
+            .WithMany()
+            .HasForeignKey(x => x.MemberId);
+    }
+}
